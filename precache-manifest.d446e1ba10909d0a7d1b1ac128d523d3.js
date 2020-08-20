c6665e1e51fd0025e426073c42a5445f4fb6a386
Vimean Chea <vimeanc@csu.fullerton.edu>
2020-08-20 11:02:05 -0700

    Updates

---
+++ precache-manifest.d446e1ba10909d0a7d1b1ac128d523d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9586a397ccbed4662483f5663d53935",
+    "url": "/vchea/robotFriends/index.html"
+  },
+  {
+    "revision": "959f82a45bc5bbc0dcc1",
+    "url": "/vchea/robotFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1348391ae9560a5b4e95",
+    "url": "/vchea/robotFriends/static/css/main.c5015e50.chunk.css"
+  },
+  {
+    "revision": "959f82a45bc5bbc0dcc1",
+    "url": "/vchea/robotFriends/static/js/2.43e7e056.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vchea/robotFriends/static/js/2.43e7e056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1348391ae9560a5b4e95",
+    "url": "/vchea/robotFriends/static/js/main.0b1da894.chunk.js"
+  },
+  {
+    "revision": "f98502416a3906a63d36",
+    "url": "/vchea/robotFriends/static/js/runtime-main.5b5ecb65.js"
+  },
+  {
+    "revision": "91ba84add59d509e37ceb41042c071e2",
+    "url": "/vchea/robotFriends/static/media/Tadeliji.91ba84ad.woff"
+  }
+]);
